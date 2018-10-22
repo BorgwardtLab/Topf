69c0e1a4756336d3752e1eb043eb464387ab9a82
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-10-22 11:12:34 +0200

    Added basic `setup.py`

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='topf',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy'],
+
+    package_data={
+        # Not sure whether it is smart to distribute the logo along with
+        # the package, but here we go...
+        '': ['topf.svg'],
+    },
+
+    author='Bastian Rieck',
+    author_email='bastian.rieck@bsse.ethz.ch',
+    description='A package for performing topological peak filtering',
+    license='MIT',
+    keywords='topology tda peak peak-filtering',
+    url='https://github.com/BorgwardtLab/topf'
+)
